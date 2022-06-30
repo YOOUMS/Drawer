654bf6e279da10143c649183e024163f7dd40922
youms <yussef.msabeh@gmail.com>
2022-06-30 11:40:36 +0300

    modified things

--- lib/simpleUI.dart
+++ lib/simpleUI.dart
@@ -6,6 +6,54 @@ import 'package:madiai_query/widgetDrawer.dart';
 class SimpleUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DrawerWidget();
+    final isLandSape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    if (isLandSape) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Text("Media Query"),
+            centerTitle: true,
+          ),
+          body: DrawerWidget());
+    }
+    return Scaffold(
+      drawer: Drawer(
+          child: Column(
+        children: [
+          const UserAccountsDrawerHeader(
+              decoration: (BoxDecoration(color: Colors.green)),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/67508677?v=4"),
+              ),
+              accountName: Text("Yusuf Abu Msabeh"),
+              accountEmail: Text("yussef.msabeh@gmail.com")),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+            trailing: Icon(Icons.arrow_forward_ios),
+            subtitle: Text("Go Home"),
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text("Favorite"),
+            trailing: Icon(Icons.arrow_forward_ios),
+            subtitle: Text("Your Favorite Products"),
+          ),
+          ListTile(
+            leading: Icon(Icons.perm_identity),
+            title: Text("profile"),
+            trailing: Icon(Icons.arrow_forward_ios),
+            subtitle: const Text("Details"),
+          ),
+        ],
+      )),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Media Query"),
+        centerTitle: true,
+      ),
+    );
   }
 }
--- lib/widgetDrawer.dart
+++ lib/widgetDrawer.dart
@@ -9,93 +9,46 @@ class DrawerWidget extends StatelessWidget {
     final isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
 
-    if (isLandscape) {
-      return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green,
-            title: Text("Drawer"),
-            centerTitle: true,
-          ),
-          body: Row(
+    return Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width / 3,
+          child: ListView(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width / 3,
-                child: ListView(
-                  children: [
-                    Column(
-                      children: [
-                        const UserAccountsDrawerHeader(
-                            decoration: (BoxDecoration(color: Colors.green)),
-                            currentAccountPicture: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/67508677?v=4"),
-                            ),
-                            accountName: Text("Yusuf Abu Msabeh"),
-                            accountEmail: Text("yussef.msabeh@gmail.com")),
-                        ListTile(
-                          leading: Icon(Icons.home),
-                          title: Text("Home"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                          subtitle: Text("Go Home"),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.favorite),
-                          title: Text("Favorite"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                          subtitle: Text("Your Favorite Products"),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.perm_identity),
-                          title: Text("profile"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                          subtitle: const Text("Details"),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ));
-    }
-
-    return Scaffold(
-      drawer: Drawer(
-          child: Column(
-        children: [
-          const UserAccountsDrawerHeader(
-              decoration: (BoxDecoration(color: Colors.green)),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/67508677?v=4"),
+              Column(
+                children: [
+                  const UserAccountsDrawerHeader(
+                      decoration: (BoxDecoration(color: Colors.green)),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/67508677?v=4"),
+                      ),
+                      accountName: Text("Yusuf Abu Msabeh"),
+                      accountEmail: Text("yussef.msabeh@gmail.com")),
+                  ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text("Home"),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    subtitle: Text("Go Home"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.favorite),
+                    title: Text("Favorite"),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    subtitle: Text("Your Favorite Products"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.perm_identity),
+                    title: Text("profile"),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    subtitle: const Text("Details"),
+                  ),
+                ],
               ),
-              accountName: Text("Yusuf Abu Msabeh"),
-              accountEmail: Text("yussef.msabeh@gmail.com")),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            subtitle: Text("Go Home"),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text("Favorite"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            subtitle: Text("Your Favorite Products"),
-          ),
-          ListTile(
-            leading: Icon(Icons.perm_identity),
-            title: Text("profile"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            subtitle: const Text("Details"),
-          ),
-        ],
-      )),
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("Media Query"),
-        centerTitle: true,
-      ),
+        )
+      ],
     );
   }
 }
